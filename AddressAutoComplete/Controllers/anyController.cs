3a19d49f2ef5f454f8ed27bed33f19d429beabea
Adrien Ribault <ribaultadrien@orange.fr>
2023-10-27 18:11:11 +0200

    add docker-compose

---
+++ AddressAutoComplete/Controllers/anyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OsmSharp.Streams;
+using System;
+using System.Diagnostics; // Add this for Stopwatch
+using System.IO;
+
+namespace AdressAutoComplete.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AnyController : ControllerBase
+    {
+        /// <summary>
+        /// Gets PBF data based on the provided query and limit.
+        /// </summary>
+        /// <param name="q">The search query.</param>
+        /// <param name="limit">The limit of results to return.</param>
+        /// <returns>Returns a list of OSM data based on the search query.</returns>
+        [HttpGet]
+        public IActionResult GetPbfData([FromQuery] string q, [FromQuery] int limit)
+        {
+            string fileName = "bretagne-latest.osm.pbf";
+            string filePath = Path.Combine("osmFiles", fileName);
+
+            try
+            {
+                using var fileStream = System.IO.File.OpenRead(filePath);
+                var source = new PBFOsmStreamSource(fileStream);
+                var filteredCities = from osmGeo in source
+                                     where osmGeo.Tags.ContainsKey("name") && osmGeo.Tags["name"].ToLower().Contains(q.Trim().ToLower())
+                                     || osmGeo.Tags.Any(tag => tag.Value.ToLower().Contains(q.Trim().ToLower()))
+                                     select osmGeo;
+
+                var osmData = filteredCities.Distinct().Take(limit).ToList();
+
+                return Ok(osmData);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
--- AddressAutoComplete/README.md
+++ AddressAutoComplete/README.md
@@ -1,10 +1,24 @@
-# API AddressAutoComplete
+﻿# API AddressAutoComplete
 >AddressAutoComplete API populates an input (country,city,address) field with relevant address suggestions.
 
 - ## Documentation
 	- [Postman Collection](AddressAutoComplete.postman_collection.json)
 	- Swagger : ```https://localhost:PORT/swagger/index.html```
 
-- ## Require
-	- a osm.pbf files from  [GeoFrabrik.de](https://download.geofabrik.de/)
+- ## Local deployment
+	- ## Requirements
+		- a osm.pbf files from  [GeoFrabrik.de](https://download.geofabrik.de/)
 		- download one and add it to ./osmFiles folder, recommend to not download a big files for test (ex: [Bretagne ~280MB](https://download.geofabrik.de/europe/france/bretagne.html))
+	
+	- ```docker-compose up```		
+- ## Roadmap
+	- ### Ameliorations
+		- import specific des données du fichier pbf dans postgres avec postGIS.	 
+		- Indexation des données : arbres de recherche ou index spatiaux.
+		- Parallélisation: paralléliser le filtrage des entités
+		- Caching: mise en cache pour éviter de refaire les même recherche fréquemment.
+		- Optimisation de la requéte : utiliser des méthodes plus rapides
+		- Optimisation de la lecture de fichiers. 
+		- Utilisation de l'API Overpass 
+		- Profilage du code
+		- Gestion des exceptions
